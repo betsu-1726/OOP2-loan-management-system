4413f94855dd5357c40671ca35b4e6ba049dd077
nayoti <56726277+nayoti@users.noreply.github.com>
2022-01-06 17:24:18 +0300

    Add files via upload

---
+++ Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManagmentSystem
+{
+    class Home
+    {
+        public Home()
+        {
+            Console.WriteLine("Home Running");
+        }
+    }
+}
