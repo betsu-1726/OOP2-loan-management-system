b896f1b8d7137501b9396700799a32092b930c41
Betselot A <58970427+betsu-1726@users.noreply.github.com>
2022-01-06 12:05:07 +0300

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManagmentSystem
+{
+    class Menu
+    {
+        
+        public void menu1()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t=================| WELCOME TO |===============");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.Home | ");
+            Console.Write(" 2.Loans |");
+            Console.Write(" 3.Payments |");
+            Console.Write(" 4.Loan Plans |");
+            Console.Write(" 5.Loan types |");
+            Console.Write(" 6.Customers |");
+            Console.Write(" 6.Report |");
+            Console.WriteLine(" 7.Exit |");
+            Console.WriteLine("===================================================================================================");
+        }
+        public void menu2()
+        {
+
+        }
+        public void menu3()
+        {
+
+        }
+        public void menu4()
+        {
+
+        }
+        public void menu5()
+        {
+
+        }
+    }
+}
