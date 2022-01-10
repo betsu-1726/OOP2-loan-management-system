29e89d26b219f288bfe5f1ae32b80bd88db5ad26
kakacr7 <45389709+kakacr7@users.noreply.github.com>
2022-01-10 16:35:38 +0300

    Add files via upload

--- Menu.cs
+++ Menu.cs
@@ -21,24 +21,95 @@ namespace LoanManagmentSystem
             Console.Write(" 4.Loan Plans |");
             Console.Write(" 5.Loan types |");
             Console.Write(" 6.Customers |");
+            Console.Write(" 7.Report |");
+            Console.WriteLine(" 8.Exit |");
+            Console.WriteLine("===================================================================================================");
+        }
+
+
+
+        public void customerMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|          Customer        |==========");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.View Customers | ");
+            Console.WriteLine("2.Return To Menu |");
+            Console.WriteLine("===================================================================================================");
+
+        }
+        public void loanMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|           Loan           |==========");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.New Loan Application | ");
+            Console.Write(" 2.Loans |");
+            Console.Write(" 3.Payments |");
+            Console.Write(" 4.Loan Plans |");
+            Console.Write(" 5.Loan types |");
+            Console.Write(" 6.Customers |");
             Console.Write(" 6.Report |");
             Console.WriteLine(" 7.Exit |");
             Console.WriteLine("===================================================================================================");
-        }
-        public void menu2()
-        {
 
         }
-        public void menu3()
+        public void loanPlanMenu()
         {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|        Loan Plan         |==========");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.Home | ");
+            Console.Write(" 2.Loans |");
+            Console.Write(" 3.Payments |");
+            Console.Write(" 4.Loan Plans |");
+            Console.Write(" 5.Loan types |");
+            Console.Write(" 6.Customers |");
+            Console.Write(" 6.Report |");
+            Console.WriteLine(" 7.Exit |");
+            Console.WriteLine("===================================================================================================");
 
         }
-        public void menu4()
+        public void loanTypeMenu()
         {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|        Loan Type         |==========");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.Home | ");
+            Console.Write(" 2.Loans |");
+            Console.Write(" 3.Payments |");
+            Console.Write(" 4.Loan Plans |");
+            Console.Write(" 5.Loan types |");
+            Console.Write(" 6.Customers |");
+            Console.Write(" 6.Report |");
+            Console.WriteLine(" 7.Exit |");
+            Console.WriteLine("===================================================================================================");
 
         }
-        public void menu5()
+        public void paymentMenu()
         {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|         Payment          |==========");
+            Console.WriteLine("===================================================================================================");
+            Console.Write("| 1.Home | ");
+            Console.Write(" 2.Loans |");
+            Console.Write(" 3.Payments |");
+            Console.Write(" 4.Loan Plans |");
+            Console.Write(" 5.Loan types |");
+            Console.Write(" 6.Customers |");
+            Console.Write(" 6.Report |");
+            Console.WriteLine(" 7.Exit |");
+            Console.WriteLine("===================================================================================================");
 
         }
     }
