ac7c6e64a9637c17dc5e91bc62f7f3403e95dca4
Betselot A <58970427+betsu-1726@users.noreply.github.com>
2022-01-13 11:01:48 +0300

    Add files via upload

--- LoanType.cs
+++ LoanType.cs
@@ -9,8 +9,48 @@ namespace LoanManagmentSystem
     {
         public string loanType;
         public string loanDescription;
+        public Boolean GoOut = false;
+
         public LoanType() {
-        
+            try
+            {
+                MenuPoint:
+
+                new Menu().loanTypeMenu();
+
+                Console.Write("Select Service : _\b");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        createLoanType();
+                        break;
+                    case "2":
+                        listLoanType();
+                        Console.WriteLine("Press Enter To Continue...");
+                        Console.Read();
+                        break;
+                    case "3":
+                        GoOut = true;
+                        break;
+                    default:
+                        break;
+                }
+                
+                if (!GoOut)
+                {
+                    goto MenuPoint;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Database Empty or Error May Occured.Hit Enter to Continue...");
+                Console.Read();
+                Console.Read();
+
+            }
         }
 
         public void createLoanType()
@@ -48,25 +88,26 @@ namespace LoanManagmentSystem
                     //save data as a row
                     var line = loanType + "|" + loanDescription;
                     File.AppendAllText(@"./loan_type.txt", line + Environment.NewLine);
-                    Console.WriteLine("Saved");
+                    Console.WriteLine("Loan Type Has Been Saved.Please Hit Enter To Continue...");
                     break;
                 default:
-                    Console.WriteLine("Not Saved");
+                    Console.WriteLine("Loan Plan Has Been Discarded.Please Hit Enter To Continue...");
                     break;
             }
 
+            Console.Read();
 
 
         }
 
-        public void listLoanType()
+        public static void listLoanType()
         {
             //#
             //loan type
             //description
 
             Console.WriteLine("---------------------------------------------------------------------------------");
-            Console.WriteLine("| ID |\tLoan Type\t\t|\t\t\t Loan Description");
+            Console.WriteLine("| {0,-4} | {1,-20} | {2}", "ID", "Loan Type","Loan Description");
             Console.WriteLine("---------------------------------------------------------------------------------");
             List<string> rows = new List<string>();
             var tempoLine = "";
@@ -87,19 +128,19 @@ namespace LoanManagmentSystem
 
             }
 
+            if (File.ReadAllText("./loan_type.txt").Length == 0)
+            {
+                Console.WriteLine("No Database");
+            }
+
             var counter = 0;
             foreach (string line in rows)
             {
                 counter++;
                 var eachLine = line.Split("|", StringSplitOptions.RemoveEmptyEntries);
-                Console.WriteLine("| {0} | \t {1} \t\t\t {2}", counter, eachLine[0], eachLine[1]);
+                Console.WriteLine("| {0,-4} | {1,-20} |  {2}", counter, eachLine[0], eachLine[1]);
             }
             Console.WriteLine("---------------------------------------------------------------------------------");
-
-
-
-
-
         }
 
     }
--- Menu.cs
+++ Menu.cs
@@ -25,8 +25,21 @@ namespace LoanManagmentSystem
             Console.WriteLine(" 8.Exit |");
             Console.WriteLine("===================================================================================================");
         }
+        public void HomeMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|          Home        |==============");
+            Console.WriteLine("======================================================================================================================================");
+            Console.Write("| 1.View Total WithDrawal Money Today | ");
+            Console.Write("2.Total Loan Requests  | ");
+            Console.Write("3.Total Recived Money Today | ");
+            Console.Write("4.Active Customers | ");
+            Console.WriteLine("5.Return To Menu |");
+            Console.WriteLine("======================================================================================================================================");
 
-
+        }
 
         public void customerMenu()
         {
@@ -46,16 +59,13 @@ namespace LoanManagmentSystem
             Console.WriteLine("\t\t\t==============================================");
             Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
             Console.WriteLine("\t\t\t========|           Loan           |==========");
-            Console.WriteLine("===================================================================================================");
+            Console.WriteLine("================================================================================================================");
             Console.Write("| 1.New Loan Application | ");
-            Console.Write(" 2.Loans |");
-            Console.Write(" 3.Payments |");
-            Console.Write(" 4.Loan Plans |");
-            Console.Write(" 5.Loan types |");
-            Console.Write(" 6.Customers |");
-            Console.Write(" 6.Report |");
-            Console.WriteLine(" 7.Exit |");
-            Console.WriteLine("===================================================================================================");
+            Console.Write("2.View Loan History |");
+            Console.Write("3.View Approved Loans |");
+            Console.Write("4.View Rejected Loans |");
+            Console.WriteLine("5.Return To Menu |");
+            Console.WriteLine("================================================================================================================");
 
         }
         public void loanPlanMenu()
@@ -65,16 +75,11 @@ namespace LoanManagmentSystem
             Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
             Console.WriteLine("\t\t\t========|        Loan Plan         |==========");
             Console.WriteLine("===================================================================================================");
-            Console.Write("| 1.Home | ");
-            Console.Write(" 2.Loans |");
-            Console.Write(" 3.Payments |");
-            Console.Write(" 4.Loan Plans |");
-            Console.Write(" 5.Loan types |");
-            Console.Write(" 6.Customers |");
-            Console.Write(" 6.Report |");
-            Console.WriteLine(" 7.Exit |");
+            Console.Write("| 1.Create New Plan | ");
+            Console.Write("2.View Loan Plans |");           
+            Console.WriteLine("3.Return To Menu |");
             Console.WriteLine("===================================================================================================");
-
+            
         }
         public void loanTypeMenu()
         {
@@ -83,14 +88,9 @@ namespace LoanManagmentSystem
             Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
             Console.WriteLine("\t\t\t========|        Loan Type         |==========");
             Console.WriteLine("===================================================================================================");
-            Console.Write("| 1.Home | ");
-            Console.Write(" 2.Loans |");
-            Console.Write(" 3.Payments |");
-            Console.Write(" 4.Loan Plans |");
-            Console.Write(" 5.Loan types |");
-            Console.Write(" 6.Customers |");
-            Console.Write(" 6.Report |");
-            Console.WriteLine(" 7.Exit |");
+            Console.Write("| 1.Create New Loan Type | ");
+            Console.Write("2.View Loan Types |");
+            Console.WriteLine("3.Return To Menu |");
             Console.WriteLine("===================================================================================================");
 
         }
@@ -100,16 +100,27 @@ namespace LoanManagmentSystem
             Console.WriteLine("\t\t\t==============================================");
             Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
             Console.WriteLine("\t\t\t========|         Payment          |==========");
-            Console.WriteLine("===================================================================================================");
-            Console.Write("| 1.Home | ");
-            Console.Write(" 2.Loans |");
-            Console.Write(" 3.Payments |");
-            Console.Write(" 4.Loan Plans |");
-            Console.Write(" 5.Loan types |");
-            Console.Write(" 6.Customers |");
-            Console.Write(" 6.Report |");
-            Console.WriteLine(" 7.Exit |");
-            Console.WriteLine("===================================================================================================");
+            Console.WriteLine("==================================================================================================================================");
+            Console.Write("| 1.Make Payment | ");
+            Console.Write("2.View All Payment Transaction |");
+            Console.Write("3.View Users Loan History |");
+            Console.Write("4.Check Customer Loan Clearance |");
+            Console.WriteLine("5.Returen To Menu |");
+            Console.WriteLine("==================================================================================================================================");
+
+        }
+
+        public void reportMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t\t==============================================");
+            Console.WriteLine("\t\t\t========|  LOAN MANAGEMENT SYSTEM  |==========");
+            Console.WriteLine("\t\t\t========|         Report           |==========");
+            Console.WriteLine("==================================================================================================================================");
+            Console.Write("| 1.View Monthly Withdrawn Loan | ");
+            Console.Write("2.View Monthly Paid Loan |");
+            Console.WriteLine("3.Returen To Menu |");
+            Console.WriteLine("==================================================================================================================================");
 
         }
     }
